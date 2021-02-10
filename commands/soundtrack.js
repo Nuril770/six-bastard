47af1af824fa5696f7e56966c6d49ad380a22fa0
Wahyu <75367930+Nikolas-4265@users.noreply.github.com>
2021-02-11 00:49:07 +0700

    Update soundtrack.js

--- commands/soundtrack.js
+++ commands/soundtrack.js
@@ -55,6 +55,7 @@ if(args.join(" ").toLowerCase() === '1') {
             }
         })
     }).catch(e => console.log(e))
+}
 if(args.join(" ").toLowerCase() === '2') {
     message.member.voice.channel.join().then(VoiceConnection => {
         VoiceConnection.play("./soundtrack/02-Parasites.mp3").on("finish", () => VoiceConnection.disconnect({ timeout: 100000 }));
@@ -75,5 +76,445 @@ if(args.join(" ").toLowerCase() === '2') {
         })
     }).catch(e => console.log(e))
 }
+if(args.join(" ").toLowerCase() === '3') {
+    message.member.voice.channel.join().then(VoiceConnection => {
+        VoiceConnection.play("./soundtrack/03-Prison-Walls.mp3").on("finish", () => VoiceConnection.disconnect({ timeout: 100000 }));
+        message.react('👌')
+        message.channel.send({
+            embed: {
+                color: 'YELLOW',
+                author: {
+                    name: 'Memutar Soundtrack'
+                },
+                description: 'Little Nightmares Soundtrack **Prison Walls**',
+                footer: {
+                    text: client.user.username,
+                    icon_url: client.user.avatarURL()
+                },
+                timestamp: new Date()
+            }
+        })
+    }).catch(e => console.log(e))
 }
+if(args.join(" ").toLowerCase() === '4') {
+    message.member.voice.channel.join().then(VoiceConnection => {
+        VoiceConnection.play("./soundtrack/04-Hunger-I.mp3").on("finish", () => VoiceConnection.disconnect({ timeout: 100000 }));
+        message.react('👌')
+        message.channel.send({
+            embed: {
+                color: 'YELLOW',
+                author: {
+                    name: 'Memutar Soundtrack'
+                },
+                description: 'Little Nightmares Soundtrack **Hunger Part.1**',
+                footer: {
+                    text: client.user.username,
+                    icon_url: client.user.avatarURL()
+                },
+                timestamp: new Date()
+            }
+        })
+    }).catch(e => console.log(e))
+}
+if(args.join(" ").toLowerCase() === '5') {
+    message.member.voice.channel.join().then(VoiceConnection => {
+        VoiceConnection.play("./soundtrack/05-Lights-Out.mp3").on("finish", () => VoiceConnection.disconnect({ timeout: 100000 }));
+        message.react('👌')
+        message.channel.send({
+            embed: {
+                color: 'YELLOW',
+                author: {
+                    name: 'Memutar Soundtrack'
+                },
+                description: 'Little Nightmares Soundtrack **Lights Out**',
+                footer: {
+                    text: client.user.username,
+                    icon_url: client.user.avatarURL()
+                },
+                timestamp: new Date()
+            }
+        })
+    }).catch(e => console.log(e))
+}
+if(args.join(" ").toLowerCase() === '6') {
+    message.member.voice.channel.join().then(VoiceConnection => {
+        VoiceConnection.play("./soundtrack/06-The-Death-Waltz.mp3").on("finish", () => VoiceConnection.disconnect({ timeout: 100000 }));
+        message.react('👌')
+        message.channel.send({
+            embed: {
+                color: 'YELLOW',
+                author: {
+                    name: 'Memutar Soundtrack'
+                },
+                description: 'Little Nightmares Soundtrack **The Death Waltz**',
+                footer: {
+                    text: client.user.username,
+                    icon_url: client.user.avatarURL()
+                },
+                timestamp: new Date()
+            }
+        })
+    }).catch(e => console.log(e))
+}
+if(args.join(" ").toLowerCase() === '7') {
+    message.member.voice.channel.join().then(VoiceConnection => {
+        VoiceConnection.play("./soundtrack/07-Little-Lost-Things.mp3").on("finish", () => VoiceConnection.disconnect({ timeout: 100000 }));
+        message.react('👌')
+        message.channel.send({
+            embed: {
+                color: 'YELLOW',
+                author: {
+                    name: 'Memutar Soundtrack'
+                },
+                description: 'Little Nightmares Soundtrack **Little Lost Things**',
+                footer: {
+                    text: client.user.username,
+                    icon_url: client.user.avatarURL()
+                },
+                timestamp: new Date()
+            }
+        })
+    }).catch(e => console.log(e))
+}
+if(args.join(" ").toLowerCase() === '8') {
+    message.member.voice.channel.join().then(VoiceConnection => {
+        VoiceConnection.play("./soundtrack/08-Six-Theme-Part-I.mp3").on("finish", () => VoiceConnection.disconnect({ timeout: 100000 }));
+        message.react('👌')
+        message.channel.send({
+            embed: {
+                color: 'YELLOW',
+                author: {
+                    name: 'Memutar Soundtrack'
+                },
+                description: 'Little Nightmares Soundtrack **Six Theme Part.1**',
+                footer: {
+                    text: client.user.username,
+                    icon_url: client.user.avatarURL()
+                },
+                timestamp: new Date()
+            }
+        })
+    }).catch(e => console.log(e))
+}
+if(args.join(" ").toLowerCase() === '9') {
+    message.member.voice.channel.join().then(VoiceConnection => {
+        VoiceConnection.play("./soundtrack/09-The-Janitor-Awaits.mp3").on("finish", () => VoiceConnection.disconnect({ timeout: 100000 }));
+        message.react('👌')
+        message.channel.send({
+            embed: {
+                color: 'YELLOW',
+                author: {
+                    name: 'Memutar Soundtrack'
+                },
+                description: 'Little Nightmares Soundtrack **The Janitor Awaits**',
+                footer: {
+                    text: client.user.username,
+                    icon_url: client.user.avatarURL()
+                },
+                timestamp: new Date()
+            }
+        })
+    }).catch(e => console.log(e))
+}
+if(args.join(" ").toLowerCase() === '10') {
+    message.member.voice.channel.join().then(VoiceConnection => {
+        VoiceConnection.play("./soundtrack/10-The-Nomes-Nest.mp3").on("finish", () => VoiceConnection.disconnect({ timeout: 100000 }));
+        message.react('👌')
+        message.channel.send({
+            embed: {
+                color: 'YELLOW',
+                author: {
+                    name: 'Memutar Soundtrack'
+                },
+                description: 'Little Nightmares Soundtrack **The Nomes Nest**',
+                footer: {
+                    text: client.user.username,
+                    icon_url: client.user.avatarURL()
+                },
+                timestamp: new Date()
+            }
+        })
+    }).catch(e => console.log(e))
+}
+if(args.join(" ").toLowerCase() === '11') {
+    message.member.voice.channel.join().then(VoiceConnection => {
+        VoiceConnection.play("./soundtrack/11-Cold-Feet.mp3").on("finish", () => VoiceConnection.disconnect({ timeout: 100000 }));
+        message.react('👌')
+        message.channel.send({
+            embed: {
+                color: 'YELLOW',
+                author: {
+                    name: 'Memutar Soundtrack'
+                },
+                description: 'Little Nightmares Soundtrack **Cold Feet**',
+                footer: {
+                    text: client.user.username,
+                    icon_url: client.user.avatarURL()
+                },
+                timestamp: new Date()
+            }
+        })
+    }).catch(e => console.log(e))
+}
+if(args.join(" ").toLowerCase() === '12') {
+    message.member.voice.channel.join().then(VoiceConnection => {
+        VoiceConnection.play("./soundtrack/12-Cornered.mp3").on("finish", () => VoiceConnection.disconnect({ timeout: 100000 }));
+        message.react('👌')
+        message.channel.send({
+            embed: {
+                color: 'YELLOW',
+                author: {
+                    name: 'Memutar Soundtrack'
+                },
+                description: 'Little Nightmares Soundtrack **Cornered**',
+                footer: {
+                    text: client.user.username,
+                    icon_url: client.user.avatarURL()
+                },
+                timestamp: new Date()
+            }
+        })
+    }).catch(e => console.log(e))
+}
+if(args.join(" ").toLowerCase() === '13') {
+    message.member.voice.channel.join().then(VoiceConnection => {
+        VoiceConnection.play("./soundtrack/13-Moaning-Drone.mp3").on("finish", () => VoiceConnection.disconnect({ timeout: 100000 }));
+        message.react('👌')
+        message.channel.send({
+            embed: {
+                color: 'YELLOW',
+                author: {
+                    name: 'Memutar Soundtrack'
+                },
+                description: 'Little Nightmares Soundtrack **Moaning Drone**',
+                footer: {
+                    text: client.user.username,
+                    icon_url: client.user.avatarURL()
+                },
+                timestamp: new Date()
+            }
+        })
+    }).catch(e => console.log(e))
+}
+if(args.join(" ").toLowerCase() === '14') {
+    message.member.voice.channel.join().then(VoiceConnection => {
+        VoiceConnection.play("./soundtrack/14-Hunger-II.mp3").on("finish", () => VoiceConnection.disconnect({ timeout: 100000 }));
+        message.react('👌')
+        message.channel.send({
+            embed: {
+                color: 'YELLOW',
+                author: {
+                    name: 'Memutar Soundtrack'
+                },
+                description: 'Little Nightmares Soundtrack **Hungry Part.2**',
+                footer: {
+                    text: client.user.username,
+                    icon_url: client.user.avatarURL()
+                },
+                timestamp: new Date()
+            }
+        })
+    }).catch(e => console.log(e))
+}
+if(args.join(" ").toLowerCase() === '15') {
+    message.member.voice.channel.join().then(VoiceConnection => {
+        VoiceConnection.play("./soundtrack/15-A-Feeling-For-Meat.mp3").on("finish", () => VoiceConnection.disconnect({ timeout: 100000 }));
+        message.react('👌')
+        message.channel.send({
+            embed: {
+                color: 'YELLOW',
+                author: {
+                    name: 'Memutar Soundtrack'
+                },
+                description: 'Little Nightmares Soundtrack **A Feeling For Meat**',
+                footer: {
+                    text: client.user.username,
+                    icon_url: client.user.avatarURL()
+                },
+                timestamp: new Date()
+            }
+        })
+    }).catch(e => console.log(e))
+}
+if(args.join(" ").toLowerCase() === '16') {
+    message.member.voice.channel.join().then(VoiceConnection => {
+        VoiceConnection.play("./soundtrack/16-New-Arrivals.mp3").on("finish", () => VoiceConnection.disconnect({ timeout: 100000 }));
+        message.react('👌')
+        message.channel.send({
+            embed: {
+                color: 'YELLOW',
+                author: {
+                    name: 'Memutar Soundtrack'
+                },
+                description: 'Little Nightmares Soundtrack **New Arrivals**',
+                footer: {
+                    text: client.user.username,
+                    icon_url: client.user.avatarURL()
+                },
+                timestamp: new Date()
+            }
+        })
+    }).catch(e => console.log(e))
+}
+if(args.join(" ").toLowerCase() === '17') {
+    message.member.voice.channel.join().then(VoiceConnection => {
+        VoiceConnection.play("./soundtrack/17-March-Of-The-Guests.mp3").on("finish", () => VoiceConnection.disconnect({ timeout: 100000 }));
+        message.react('👌')
+        message.channel.send({
+            embed: {
+                color: 'YELLOW',
+                author: {
+                    name: 'Memutar Soundtrack'
+                },
+                description: 'Little Nightmares Soundtrack **March Of The Guests**',
+                footer: {
+                    text: client.user.username,
+                    icon_url: client.user.avatarURL()
+                },
+                timestamp: new Date()
+            }
+        })
+    }).catch(e => console.log(e))
+}
+if(args.join(" ").toLowerCase() === '18') {
+    message.member.voice.channel.join().then(VoiceConnection => {
+        VoiceConnection.play("./soundtrack/18-The-Way-Out.mp3").on("finish", () => VoiceConnection.disconnect({ timeout: 100000 }));
+        message.react('👌')
+        message.channel.send({
+            embed: {
+                color: 'YELLOW',
+                author: {
+                    name: 'Memutar Soundtrack'
+                },
+                description: 'Little Nightmares Soundtrack **The Way Out**',
+                footer: {
+                    text: client.user.username,
+                    icon_url: client.user.avatarURL()
+                },
+                timestamp: new Date()
+            }
+        })
+    }).catch(e => console.log(e))
+}
+if(args.join(" ").toLowerCase() === '19') {
+    message.member.voice.channel.join().then(VoiceConnection => {
+        VoiceConnection.play("./soundtrack/19-Hypnosis.mp3").on("finish", () => VoiceConnection.disconnect({ timeout: 100000 }));
+        message.react('👌')
+        message.channel.send({
+            embed: {
+                color: 'YELLOW',
+                author: {
+                    name: 'Memutar Soundtrack'
+                },
+                description: 'Little Nightmares Soundtrack **Hypnosis**',
+                footer: {
+                    text: client.user.username,
+                    icon_url: client.user.avatarURL()
+                },
+                timestamp: new Date()
+            }
+        })
+    }).catch(e => console.log(e))
+}
+if(args.join(" ").toLowerCase() === '20') {
+    message.member.voice.channel.join().then(VoiceConnection => {
+        VoiceConnection.play("./soundtrack/20-The-Lady-Circles.mp3").on("finish", () => VoiceConnection.disconnect({ timeout: 100000 }));
+        message.react('👌')
+        message.channel.send({
+            embed: {
+                color: 'YELLOW',
+                author: {
+                    name: 'Memutar Soundtrack'
+                },
+                description: 'Little Nightmares Soundtrack **The Lady Circles**',
+                footer: {
+                    text: client.user.username,
+                    icon_url: client.user.avatarURL()
+                },
+                timestamp: new Date()
+            }
+        })
+    }).catch(e => console.log(e))
+}
+if(args.join(" ").toLowerCase() === '21') {
+    message.member.voice.channel.join().then(VoiceConnection => {
+        VoiceConnection.play("./soundtrack/21-Hunger-III.mp3").on("finish", () => VoiceConnection.disconnect({ timeout: 100000 }));
+        message.react('👌')
+        message.channel.send({
+            embed: {
+                color: 'YELLOW',
+                author: {
+                    name: 'Memutar Soundtrack'
+                },
+                description: 'Little Nightmares Soundtrack **Hungry Part.3**',
+                footer: {
+                    text: client.user.username,
+                    icon_url: client.user.avatarURL()
+                },
+                timestamp: new Date()
+            }
+        })
+    }).catch(e => console.log(e))
+}
+if(args.join(" ").toLowerCase() === '22') {
+    message.member.voice.channel.join().then(VoiceConnection => {
+        VoiceConnection.play("./soundtrack/22-Six-Theme-Part-II.mp3").on("finish", () => VoiceConnection.disconnect({ timeout: 100000 }));
+        message.react('👌')
+        message.channel.send({
+            embed: {
+                color: 'YELLOW',
+                author: {
+                    name: 'Memutar Soundtrack'
+                },
+                description: 'Little Nightmares Soundtrack **Six Theme Part.2**',
+                footer: {
+                    text: client.user.username,
+                    icon_url: client.user.avatarURL()
+                },
+                timestamp: new Date()
+            }
+        })
+    }).catch(e => console.log(e))
+}
+if(args.join(" ").toLowerCase() === '23') {
+    message.member.voice.channel.join().then(VoiceConnection => {
+        VoiceConnection.play("./soundtrack/23-Beneath-The-Waves.mp3").on("finish", () => VoiceConnection.disconnect({ timeout: 100000 }));
+        message.react('👌')
+        message.channel.send({
+            embed: {
+                color: 'YELLOW',
+                author: {
+                    name: 'Memutar Soundtrack'
+                },
+                description: 'Little Nightmares Soundtrack **Beneath The Waves**',
+                footer: {
+                    text: client.user.username,
+                    icon_url: client.user.avatarURL()
+                },
+                timestamp: new Date()
+            }
+        })
+    }).catch(e => console.log(e))
+}
+if(args.join(" ").toLowerCase() === '24') {
+    message.member.voice.channel.join().then(VoiceConnection => {
+        VoiceConnection.play("./soundtrack/24-Prison-Toys.mp3").on("finish", () => VoiceConnection.disconnect({ timeout: 100000 }));
+        message.react('👌')
+        message.channel.send({
+            embed: {
+                color: 'YELLOW',
+                author: {
+                    name: 'Memutar Soundtrack'
+                },
+                description: 'Little Nightmares Soundtrack **Prison Toys**',
+                footer: {
+                    text: client.user.username,
+                    icon_url: client.user.avatarURL()
+                },
+                timestamp: new Date()
+            }
+        })
+    }).catch(e => console.log(e))
+}
+}//
 }
